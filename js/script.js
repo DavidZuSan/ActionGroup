41e6ad76145f0a20887bb673d4b500c70a9e6349
DavidZuSan <lacosadelumbral@gmail.com>
2023-05-28 16:04:31 +0200

    Primeros pasos
    Menú creado (pero hay que mejorarlo). Y algunos detalles del header

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var trm = document.getElementById('toggleRightMenu')
-var navWrap = document.getElementById('navWrap')
-trm.onclick = function(){
-  if(navWrap.className === 'hideToggle') navWrap.className = ''
-  else navWrap.className = 'hideToggle'
-}
